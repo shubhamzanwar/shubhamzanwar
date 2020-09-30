1a6c1f39c20092abfd33dd953e597ae3b42207ad
Shubham Zanwar <zanwar.shubham@gmail.com>
2020-09-30 19:31:43 +0530

    changing blog alert message

--- src/components/Blog/BlogAlert.vue
+++ src/components/Blog/BlogAlert.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="spacer">
         <alert type="info">
-            All of these articles are available on 
-            <a href="https://github.com/shubhamzanwar" target="_blank">medium</a>. I also soon plan to start writing on
+            I recently shifted my blog to this website. You can find my older posts on
+            <a href="https://github.com/shubhamzanwar" target="_blank">medium</a> and 
             <a href="https://dev.to/shubhamzanwar" target="_blank">Dev.to</a>.
         </alert>
     </div>
